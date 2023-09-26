8ea00ea57de1e7e9571d0a5658d323895219a994
drzamora <richard.zamora2010@gmail.com>
2023-09-26 02:41:24 -0600

    Stage 4 Test Automatizados Junit5

---
+++ src/main/resources/db/migration/V8__insert-into-table-pacientes.sql
@@ -0,0 +1,14 @@
+DELETE FROM `vollmed_api`.`pacientes`;
+
+INSERT INTO `vollmed_api`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('1', 'carla', 'c@mail.com', '3333333', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+INSERT INTO `vollmed_api`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('2', 'debora', 'd@mail.com', '4444444', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+INSERT INTO `vollmed_api`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('3', 'esmeralda', 'e@mail.com', '5555555', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+INSERT INTO `vollmed_api`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('4', 'francis', 'f@mail.com', '6666666', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+INSERT INTO `vollmed_api`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('5', 'amanda', 'a@mail.com', '7777777', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
+INSERT INTO `vollmed_api`.`pacientes` (`id`, `nombre`, `email`, `documento`, `calle`, `distrito`, `complemento`, `numero`, `ciudad`, `telefono`, `activo`)
+VALUES ('6', 'bruna', 'b@mail.com', '8888888', 'ran', 'ran', '900', '900', 'ran', '12334567', '1');
